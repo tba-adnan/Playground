c568e59a63e7ecfb7ade22cfaa35c8bc1fb29c59
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-02-06 10:29:10 +0100

    fix. (functions3)

--- Kotlin-autoformation/functions/functions3.kt
+++ Kotlin-autoformation/functions/functions3.kt
@@ -1,6 +1,14 @@
 // default value.
 
-fun main() { demo() }
+fun main() {
+ demo() // normal function call with default parameter.
+ demotwo("100km/h") // specified parameter.
+ //demothree() // compact functions.
+}
 fun demo(name: String = "adnane") {
  println(name)
+}
+
+fun demotwo(speed: String = "90 km/h") {
+ println(speed)
 }
